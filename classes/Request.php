b231ad80f950db65987a8071362694ee1cddec90
Mikhail Aloshyn <reklama.alatyr@gmail.com>
2021-06-20 15:39:41 +0300

    feat: added Request class

--- classes/Request.php
+++ classes/Request.php
@@ -1,19 +1,36 @@
 <?php
 
-    namespace classes;
-
     class Request 
     {
+        private $host;
+        private $uri;
+        private $method;
+        private $getParams;
+        private $postParams;
+        private $resource;
+        private $arguments;
+
         function __construct()
         {
             $this->host = $_SERVER['HTTP_HOST'];
             $this->uri = $_SERVER['REQUEST_URI'];
             $this->method = $_SERVER['REQUEST_METHOD'];
-            $this->query = $_SERVER['QUERY_STRING'];
+            $this->getParams = $_GET;
+            $this->postParams = $_POST;
+            $argString = explode('?', $this->uri)[0];
+            if (strlen($argString) > 5)
+            {
+                $argArray = explode('/', substr(rtrim($argString, "/"), 5));
+                $this->resource = ucfirst($argArray[0]);
+                if (count($argArray) > 1)
+                {
+                    $this->arguments = array_slice($argArray, 1);
+                }
+            }
         }
 
-        function printRequest() 
+        public function __get($attr)
         {
-            var_dump($this);
+            return $this->{$attr};
         }
     }
